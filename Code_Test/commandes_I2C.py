cc9c90b5c88bd2e6fc765ccc7363c91bb913722e
AdamDubois <adamdubois19@hotmail.com>
2026-01-17 13:30:01 -0500

    Auto-commit du sam 17 jan 2026 13:30:01 EST

---
+++ Code_Test/commandes_I2C.py
@@ -0,0 +1,24 @@
+import smbus2      # Pour l'I2C (sudo pip install smbus2)
+import time
+
+# Adresse I2C de l'ESP32 (doit correspondre à SLAVE_ADDR dans ton Arduino)
+ESP32_ADDR = 0x0A  # (0x0a en hex, 10 en décimal)
+I2C_BUS = 1        # Bus I2C habituels sur Pi (bus 1 sur la plupart des modèles)
+
+bus = smbus2.SMBus(I2C_BUS)
+
+print("Envoi de message I2C vers ESP32, Ctrl+C pour quitter.")
+try:
+    while True:
+        user_msg = input("Message à envoyer (texte, ex: TEST): ")
+        # On convertit la chaîne en liste d'octets (bytearray), limite: 32 à 128 octets selon le MCU
+        data = bytearray(user_msg, "utf-8")
+        # Envoi sur le bus (tous les octets sauf \0 final)
+        for i in range(0, len(data), 30):  # Envoie par paquets de 30 pour être compatible
+            bus.write_i2c_block_data(ESP32_ADDR, 0, list(data[i:i+30]))
+            time.sleep(0.05)  # Laisse le temps au microcontrôleur d'avaler le block
+        print("Envoyé!")
+except KeyboardInterrupt:
+    print("\nFin du programme !")
+finally:
+    bus.close()
