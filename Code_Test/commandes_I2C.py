17a36f9f7dd4c85777e183612a0f8337bd7d8143
AdamDubois <adamdubois19@hotmail.com>
2026-01-28 09:30:01 -0500

    Auto-commit du mer 28 jan 2026 09:30:01 EST

--- Code_Test/commandes_I2C.py
+++ Code_Test/commandes_I2C.py
@@ -1,11 +1,11 @@
-import smbus2      # Pour l'I2C (sudo pip install smbus2)
+from smbus2 import SMBus, i2c_msg      # Pour l'I2C (sudo pip install smbus2)
 import time
 
 # Adresse I2C de l'ESP32 (doit correspondre à SLAVE_ADDR dans ton Arduino)
 ESP32_ADDR = 0x10  # (0x0a en hex, 10 en décimal)
 I2C_BUS = 1        # Bus I2C habituels sur Pi (bus 1 sur la plupart des modèles)
 
-bus = smbus2.SMBus(I2C_BUS)
+bus = SMBus(I2C_BUS)
 
 print("Envoi de message I2C vers ESP32, Ctrl+C pour quitter.")
 try:
@@ -24,11 +24,17 @@ try:
         # On attend une réponse
         time.sleep(0.1)  # Petit délai avant de lire la réponse
         try:
-            # Lecture de la réponse (max 32 octets)
-            response = bus.read_i2c_block_data(ESP32_ADDR, 0, 32)
+            strReceived = ""
+            # Lecture de la réponse
+            response = i2c_msg.read(ESP32_ADDR, 256)  # Lire jusqu'à 256 octets
+            bus.i2c_rdwr(response)
+
+            for value in response:
+                if (value == 0x00):
+                    break
+                strReceived += chr(value)
             # Convertir la liste d'octets en chaîne
-            response_str = ''.join([chr(b) for b in response if b != 0])
-            print("Réponse reçue:", response_str)
+            print("Réponse reçue:", strReceived)
         except Exception as e:
             print("Erreur de lecture I2C:", e)
 except KeyboardInterrupt:
