c724d4ec24b8c5b365fbc8e4dfb4306101708bb0
AdamDubois <adamdubois19@hotmail.com>
2026-02-05 12:30:01 -0500

    Auto-commit du jeu 05 fév 2026 12:30:01 EST

--- Code_Test/commande_I2C_test_led.py
+++ Code_Test/commande_I2C_test_led.py
@@ -9,7 +9,7 @@ bus = SMBus(I2C_BUS)
 
 partie1 = "{V:\"\",Str:"
 partie2 = ",Cmd:\"Stat\",Nbr:40,Deb:0,Coul:\""
-partie3 = "\",Br:150}"
+partie3 = "\",Br:255}"
 couleurs = ["RED", "GREEN", "BLUE", "YELLOW", "MAGENTA", "CYAN", "WHITE"]
 
 boucle = 0
